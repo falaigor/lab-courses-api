503209d99513dd1892b382fc70f28c6ee70c0eb3
Igor Santos <falaigors@gmail.com>
2022-11-08 09:25:58 -0300

    connect ClassesModule to AppModule

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { CoursesModule } from './courses/courses.module';
+import { ClassesModule } from './classes/classes.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, CoursesModule],
+  imports: [AuthModule, UsersModule, CoursesModule, ClassesModule],
   controllers: [],
   providers: [],
 })
